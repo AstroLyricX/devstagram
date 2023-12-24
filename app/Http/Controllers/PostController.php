6f900935182acb24e93155ce5e36ff0e96e3a8fd
Ever Ronaldo <ever.condori@upeu.edu.pe>
2023-12-23 21:44:03 -0500

    create 93 files

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        // dd('Desde Muro');
+        // dd(auth()->user());
+        return view('dashboard');
+    }
+}
